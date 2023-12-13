852b4aea363de8bc1fa86d89997726547ce81178
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-13 10:32:10 +0545

    feat: closure-concept added

---
+++ closure-concept.js
@@ -0,0 +1,46 @@
+/*
+- Closure is almost same as scope. Inner function has access to outer function scope. But closure is more than that.
+- Closure is a function that has access to its outer function scope even after the outer function has returned.
+*/
+
+/ ********** Simple scope Concept ********** /;
+const globarVariable = "I am global variable";
+function outerFunction() {
+  const outerVariable = "I am outer variable";
+  function innerFunction() {
+    const innerVariable = "I am inner variable";
+    console.log(innerVariable);
+    console.log(outerVariable);
+    console.log(globarVariable);
+  }
+  innerFunction();
+}
+outerFunction();
+
+/ ********** CLOSURE Concept 1 ********** /;
+function getGreeter() {
+  console.log("getGreeter is running");
+  let name = "Marie";
+  console.log("getGreeter is finishing");
+  return function () {
+    console.log("Hi", name);
+  };
+}
+let greeter = getGreeter();
+greeter();
+
+/ ********** CLOSURE Concept 2 ********** /;
+function callCount() {
+  let counter = 0;
+  return function () {
+    counter++;
+    return counter;
+  };
+}
+
+let returnedFunction = callCount();
+
+returnedFunction();
+returnedFunction();
+let finalCount = returnedFunction();
+console.log(finalCount); // => 3
