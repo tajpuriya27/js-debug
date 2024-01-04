6eff3750a3289d6dad3bb379228df64f03ddd267
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-01-04 20:58:16 +0545

    feat: spliting method to string manipulation

--- string-manipulation.js
+++ string-manipulation.js
@@ -2,7 +2,8 @@
 original email: dummyemail2@gmail.com
 Email to show: d**********@gmail.com */
 
-const orgEmail = "dummyemail2@gmail.com";
+// const orgEmail = "dummyemail2@gmail.com";
+const orgEmail = "test@gmail.com";
 
 const regex = /(?<!^).*?(?=@)/;
 const expectedOutput = orgEmail.replace(regex, (match) =>
@@ -11,3 +12,11 @@ const expectedOutput = orgEmail.replace(regex, (match) =>
 console.log(
   "original email: " + orgEmail + "\nEmail to show: " + expectedOutput
 );
+
+let emailInArr = orgEmail.split("@");
+let firstPart = emailInArr[0];
+let secondPart = emailInArr[1];
+let emailToShow =
+  firstPart[0] + "*".repeat(firstPart.length - 1) + "@" + secondPart;
+
+console.log(emailToShow);
